8981913d1e6c8d2269b60b1502315066fb6ae8e6
mateo1761 <mateoquinterozapata@gmail.com>
2022-03-14 17:58:00 -0500

    Se modificaron nombres

--- taller1/ejercicio2.js
+++ taller1/ejercicio2.js
@@ -15,4 +15,6 @@ let recto = (codigo) => {
     return ("El nombre es: "+ nombre[1]);
 }
 
-console.log(recto("ARQ2556: mateo"));
+console.log(recto("ARQ2555: Sara Bel-sun"));
+console.log(recto("ARQ2556: Nodin Chavdri"));
+console.log(recto("ARQ2557: Finn"));
