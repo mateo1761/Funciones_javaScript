0bb2617fda117d8ed7cc3b13ff15e73d364d26fb
mateo1761 <mateoquinterozapata@gmail.com>
2022-03-20 14:37:07 -0500

    Se finaliza ejercicio 3

--- taller2/ejercicio3.js
+++ taller2/ejercicio3.js
@@ -1,3 +1,20 @@
+let nombres = ["vaca","pez","zanahoria","fresa","aguacate","grillo","mariposa","mora","lagartija","sandia"];
+let tipos = ["animal","vegetal","insectos"];
+let energias = [200,560,120,350,420,160,265,780,45,95];
+
+let alimentos = [];
+
+for(let i = 0; i < 50; i++){
+    let alimento = {};
+
+    alimento.nombre = nombres [Math.floor(Math.random() * nombres.length)];
+    alimento.tipo = tipos [Math.floor(Math.random() * tipos.length)];
+    alimento.energia = energias [Math.floor(Math.random() * energias.length)];
+
+    alimentos.push(alimento);
+}
+
+
 function entrenamiento(dietas,callback){
     setTimeout(function(){ 
 
@@ -12,70 +29,6 @@ function entrenamiento(dietas,callback){
     }, 5000)
 }
 
-let alimentos = [
-
-    {
-        nombre : "pez",
-        tipo : "animal",
-        energia : 250
-    },
-
-    {
-        nombre : "vaca",
-        tipo : "animal",
-        energia : 550
-    },
-
-    {
-        nombre : "fresa",
-        tipo : "vegetal",
-        energia : 150
-    },
-
-    {
-        nombre : "cerdo",
-        tipo : "animal",
-        energia : 300
-    },
-
-    {
-        nombre : "manzana",
-        tipo : "vegetal",
-        energia : 50
-    },
-
-    {
-        nombre : "moscas",
-        tipo : "insecto",
-        energia : 200
-    },
-
-    {
-        nombre : "huevo",
-        tipo : "animal",
-        energia : 400
-    },
-
-    {
-        nombre : "grillos",
-        tipo : "insecto",
-        energia : 250
-    },
-
-    {
-        nombre : "cangrejo",
-        tipo : "animal",
-        energia : 350
-    },
-
-    {
-        nombre : "zanahoria",
-        tipo : "vegetal",
-        energia : 150
-    },
-
-]
-
 entrenamiento(alimentos,function (tipos){
 
     let resultado = 0;
@@ -86,4 +39,4 @@ entrenamiento(alimentos,function (tipos){
 
     console.log(tipos);
     console.log("La suma de energias de la dieta de grogu es de: " +resultado);
-})
+});
